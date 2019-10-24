d068fe4e0222b22824775b96352a51f1f634cc93
Mark Reed <markdreed512@gmail.com>
2019-10-23 22:11:30 -0500

    express setup

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '/public', 'index.html'));
+})
+
+
+app.listen(3000, function(){
+    console.log("listening on port 3000!!")
+})
