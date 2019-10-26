9aa31ae0c4893024e942534a050aa1ed3b8baf8d
Mark Reed <markdreed512@gmail.com>
2019-10-25 20:47:20 -0500

    url parser

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
 app.use(express.static('public'))
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '/public', 'index.html'));
 })
+app.get('/:words', function(req, ses){
+    console.log(req.params.words)
+})
 
 
 app.listen(3000, function(){
     console.log("listening on port 3000!!")
 })
+
